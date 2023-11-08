ba6d5c055454ea6b2642810c2610dd2327ad4945
negnq <117580177+neginashkan@users.noreply.github.com>
2023-11-08 21:32:40 +0330

    set up the files

---
+++ src/Components/ProductDigital.jsx
@@ -0,0 +1,9 @@
+import "../CSS/styles.css";
+
+export default function ProductDigital() {
+    return (
+        <div className="product-digital-container">
+            <h1>Product Digital</h1>
+        </div>
+    );
+}
---
+++ src/Page.jsx
@@ -0,0 +1,14 @@
+import "./CSS/styles.css";
+import ProductDigital from "./Components/ProductDigital"
+
+
+export default function Page() {
+  return (
+    <div>
+      {/* add header here */}
+      <ProductDigital/>
+      {/* add Footer here */}
+    </div>
+  );
+}
+
