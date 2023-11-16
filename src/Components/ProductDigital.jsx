bb6d81a7796fa489d70bd2ec93711e50e6e4c3da
negnq <117580177+neginashkan@users.noreply.github.com>
2023-11-16 06:07:19 +0330

    add icon -- set up html

--- src/Components/ProductDigital.jsx
+++ src/Components/ProductDigital.jsx
@@ -3,7 +3,116 @@ import "../CSS/styles.css";
 export default function ProductDigital() {
     return (
         <div className="product-digital-container">
-            <h1>hello</h1>
+            <div className="product-digital-box">
+                <h1 className="product--tags">دیجیتال > رسته > نام شرکت سازنده > نام محصول</h1>
+                <div className="product--details--about-container">
+                    <div className="product--details">
+                        <div className="details">
+                            <div className="detail-box">
+                                <div className="detail-label">
+                                    <img src="/public/Images/blue-icon.png" alt="" className="detail--icon" />
+                                    <h4 className="detail--label">نوع محصول</h4>
+                                </div>
+                                <div className="detail-buttons">
+                                    <button className="detail-button">In-Game</button>
+                                    <button className="detail-button">Redeem Code</button>
+                                </div>
+                            </div>
+                            <div className="detail-box">
+                                <div className="detail-label">
+                                    <img src="/public/Images/blue-icon.png" alt="" className="detail--icon" />
+                                    <h4 className="detail--label">ریجن</h4>
+                                </div>
+                                <div className="detail-buttons">
+                                    <button className="detail-button">Country</button>
+                                    <button className="detail-button">Country</button>
+                                    <button className="detail-button">Country</button>
+                                </div>
+                            </div>
+                            <div className="detail-box">
+                                <div className="detail-label">
+                                    <img src="/public/Images/blue-icon.png" alt="" className="detail--icon" />
+                                    <h4 className="detail--label">مقدار (عنوان محصول) (واحد شمارش)</h4>
+                                </div>
+                                <div className="detail-buttons">
+                                    <button className="detail-button">Number</button>
+                                    <button className="detail-button">Number</button>
+                                    <button className="detail-button">Number</button>
+                                    <button className="detail-button">Number</button>
+                                    <button className="detail-button">Number</button>
+                                    <button className="detail-button">Number</button>
+                                </div>
+                            </div>
+                            <div className="detail-box">
+                                <div className="detail-label">
+                                    <img src="/public/Images/blue-icon.png" alt="" className="detail--icon" />
+                                    <h4 className="detail--label">پلتفرم</h4>
+                                </div>
+                                <div className="detail-buttons">
+                                    <button className="detail-button">Platform A</button>
+                                    <button className="detail-button">Platform B</button>
+                                    <button className="detail-button">Platform C</button>
+                                    <button className="detail-button">Platform D</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="detail-user-account">
+                            <div className="detail-label">
+                                <img src="/public/Images/blue-icon.png" alt="" className="detail--icon" />
+                                <h4 className="detail--label">اطلاعات حساب کاربری</h4>
+                            </div>
+                            <div className="detail-personal-information">
+                                <input type="text" placeholder="Username" name="" id="" className="detail-personal-information-input-style" />
+                                <input type="password" placeholder="Password" name="" id="" className="detail-personal-information-input-style" />
+                            </div>
+                        </div>
+                        <div className="detail-shopping-basket--cost">
+                            <button className="detail-shopping-basket-button">افزودن به سبد خرید</button>
+                            <p className="detail-cost">
+                                <div className="persian--cost">تومان</div>
+                                <div className="english-cost">10,000,000</div>
+                            </p>
+                        </div>
+                    </div>
+                    {/*-------------------------------------------------------------------------------------- */}
+                    <div className="product--about">
+                        <div className="product--about-game-section">
+                            <img src="/public/Images/bg.png" alt="" className="product-image" />
+                            <h2 className="product-name-heading">GAME NAME</h2>
+                            <p className="product-short-about product-p-about-style">
+                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها
+                                و متون بلکه روزنامه ومجله درستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد</p>
+                            <p className="product-long-about product-p-about-style">
+                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها
+                                و متون بلکه روزنامه ومجله درستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد
+                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها
+                                و متون بلکه روزنامه ومجله درستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد
+                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها
+                                و متون بلکه روزنامه ومجله درستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد
+                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها
+                                و متون بلکه روزنامه ومجله درستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد
+                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها
+                                و متون بلکه روزنامه ومجله درستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد
+                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها
+                                و متون بلکه روزنامه ومجله درستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد
+                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها
+                                و متون بلکه روزنامه ومجله درستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد</p>
+                        </div>
+                        <div className="product--about-comment-section">
+                            <div className="comment-section-label">
+                                <a href="#" className="other-comments">مشاهده نظرات دیگران</a>
+                                <p className="comment-section-title">نظرات</p>
+                            </div>
+                            <div className="comment-section-boxs">
+                                <input type="text" placeholder="نام" name="" id="" className="comment-section-name comment-section-input" />
+                                <input type="email" placeholder="ایمیل" name="" id="" className="comment-section-email comment-section-input" />
+                                <input type="text" placeholder="نظر شما" name="" id="" className="comment-section-comment comment-section-input" />
+                                <button className="comment-section-button">ثبت نظر</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
